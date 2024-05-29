7a4ba7a03f1c4e81ac0ec5159b0a061a64fce290
TristHund <TristanHundley@protonmail.com>
2024-05-29 20:53:37 +0300

    added about page and links to buttons on home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,7 @@
   const toggleMenu = () => {
     menuOpen.update(open => !open);
   };
+
 </script>
 
 <main class="min-h-screen bg-gray-800 text-gray-800">
@@ -55,7 +56,7 @@
       <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" class:menu-open={$menuOpen}>
         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-gray-900">
           <li>
-            <a href="/" class="block py-2 px-3 text-gray-900 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+            <a href="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
           </li>
           <li>
             <a href="/borrower" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Borrower</a>
