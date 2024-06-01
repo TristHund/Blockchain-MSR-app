58d1c3e2dfee19a42ef79d73ee8cc03d7ace4484
TristHund <TristanHundley@protonmail.com>
2024-06-01 18:32:43 +0300

    Some small config file changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: preprocess()
 };
 
 export default config;
